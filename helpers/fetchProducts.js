adcd2ea76659a00a19b7031a6daca50030b409e3
Antônio Santana <antonionetosg6@gmail.com>
2022-08-10 15:14:54 -0300

    Requisito #1, #2 [IN PROGRESS]

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,14 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = (product) => {
+  if (product !== 'computador') {
+    throw new Error('Produto inválido.')
+  }
+
+  const $QUERY = product;
+  const ENDPOINT = `https://api.mercadolibre.com/sites/MLB/search?q=${$QUERY}`
+
 };
+
+console.log(fetchProducts('computador'));
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,12 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  // fail('Teste vazio')
+  it('Testa se fetchProducts() é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('Executa a função fetchProducts() e com argumento "computador" e teste se fetch foi chamado', () => {
+    
+  })
 });
